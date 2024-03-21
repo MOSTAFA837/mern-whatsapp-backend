a6eb11cf112aeb50f8a924e7fec76ef5a62322c4
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-03-21 17:36:51 +0200

    authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import createHttpError from "http-errors";
-import { createUser } from "../services/auth.service.js";
+import { createUser, signUser } from "../services/auth.service.js";
 import { generateToken } from "../services/token.service.js";
 
 export const register = async (req, res, next) => {
@@ -45,6 +45,85 @@ export const register = async (req, res, next) => {
     });
 
     res.send(req.body);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await signUser(email, password);
+
+    const access_token = await generateToken(
+      { userId: user._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const refresh_token = await generateToken(
+      { userId: user._id },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshtoken", refresh_token, {
+      httpOnly: true,
+      path: "/api/v1/auth/refreshtoken",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
+
+    res.json({
+      message: "login success.",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        picture: user.picture,
+        status: user.status,
+        token: access_token,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = async (req, res, next) => {
+  try {
+    res.clearCookie("refreshtoken", { path: "/api/v1/auth/refreshtoken" });
+    res.json({
+      message: "logged out !",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const refreshToken = async (req, res, next) => {
+  try {
+    const refresh_token = req.cookies.refreshtoken;
+    if (!refresh_token) throw createHttpError.Unauthorized("Please login.");
+    const check = await verifyToken(
+      refresh_token,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const user = await findUser(check.userId);
+    const access_token = await generateToken(
+      { userId: user._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    res.json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        picture: user.picture,
+        status: user.status,
+        token: access_token,
+      },
+    });
   } catch (error) {
     next(error);
   }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
-// import trimRequest from "trim-request";
+import {
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/auth.controller.js";
+import trimRequest from "trim-request";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(trimRequest.all, register);
+router.route("/login").post(trimRequest.all, login);
+router.route("/logout").post(trimRequest.all, logout);
+router.route("/refreshtoken").post(trimRequest.all, refreshToken);
 
 export default router;
